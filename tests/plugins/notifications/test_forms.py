6906bf6c6f6dcb8cf68cda0e6fa0b9ed1a074bd7
Pat Browne <81719147+pbrowne011@users.noreply.github.com>
2022-10-16 06:33:38 -0400

    Delete tests directory

--- tests/plugins/notifications/test_forms.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from wiki.plugins.notifications.forms import SettingsFormSet
-
-from tests.base import RequireSuperuserMixin
-
-
-class SettingsFormTests(RequireSuperuserMixin, TestCase):
-    def test_formset(self):
-        SettingsFormSet(user=self.superuser1)
